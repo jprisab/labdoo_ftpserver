469030752e1a9bf3c4ecb443253dfb87633dce0e
jprisab <jprisab@hotmail.com>
2023-08-23 15:06:05 +0200

    Update labdoo-snap-image.sh
    comment on how to install awscli in newer versions

--- labdoo-snap-image.sh
+++ labdoo-snap-image.sh
@@ -2,12 +2,16 @@
 #CRONTABBED DAILY: 20 2 * * * /root/labdoo-snap-image.sh 2>&1 | /usr/bin/logger -t labdoo_image_snap 
 # This script creates a backup of a Labdoo AMI
 #
-# Make sure the aws cli tools are installed locally by running these commands:
+# Make sure the aws cli tools are installed locally IN THE OLDER VERSION IT WAS DONE by running these commands:
 #
 #    $ sudo apt-get update
 #    $ sudo apt-get install python-setuptools python-dev build-essential
 #    $ sudo apt-get install python-pip python-dev build-essential
 #    $ pip install --upgrade --user awscli
+
+#   But in Ubuntu 22, just 
+#    $ sudo apt-get install awscli
+
 #
 # Create a config file in ~/.aws/config similar to this:
 #
